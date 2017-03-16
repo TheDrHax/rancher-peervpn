7ef9fedc04150daf58d27a3d18ab3be14f59d521
Дмитрий Карих <the.dr.hax@gmail.com>
2017-03-17 00:02:28 +0300

    Found a fix for DNS

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "169.254.169.250 rancher-metadata.rancher.internal" >> /etc/hosts
-
 export VPN_IP="$(wget -q -O- http://rancher-metadata.rancher.internal/latest/self/host/agent_ip)/24"
 echo "Detected host IP: $VPN_IP"
 
