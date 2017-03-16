fd88e8c2233d650d34f426c8330e667e37e6d288
Дмитрий Карих <the.dr.hax@gmail.com>
2017-03-16 23:21:04 +0300

    Rancher DNS is not available with net=host

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "169.254.169.250 rancher-metadata.rancher.internal" >> /etc/hosts
+
 export VPN_IP="$(wget -O- http://rancher-metadata.rancher.internal/latest/self/host/agent_ip)/24"
 
 $@
