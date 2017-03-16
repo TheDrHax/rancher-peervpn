e830a5c53a8387318555f07a611cb307f620068d
Дмитрий Карих <the.dr.hax@gmail.com>
2017-03-16 23:30:22 +0300

    run.sh: Add -q flag to wget

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 echo "169.254.169.250 rancher-metadata.rancher.internal" >> /etc/hosts
 
-export VPN_IP="$(wget -O- http://rancher-metadata.rancher.internal/latest/self/host/agent_ip)/24"
+export VPN_IP="$(wget -q -O- http://rancher-metadata.rancher.internal/latest/self/host/agent_ip)/24"
+echo "Detected host IP: $VPN_IP"
 
 $@
