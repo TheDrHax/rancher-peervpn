d0227cc0551251f4a355106c3126f4781e18a5d2
Дмитрий Карих <the.dr.hax@gmail.com>
2018-05-17 04:19:31 +0300

    Export VPN_IP for /entrypoint.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$RANCHER_AGENT" ]; then
-    VPN_IP="$(docker inspect rancher-agent | grep CATTLE_AGENT_IP | grep -o '[0-9\.]*')/24"
+    export VPN_IP="$(docker inspect rancher-agent | grep CATTLE_AGENT_IP | grep -o '[0-9\.]*')/24"
     echo "Using variable CATTLE_AGENT_IP from $RANCHER_AGENT as VPN_IP ($VPN_IP)"
 fi
 
